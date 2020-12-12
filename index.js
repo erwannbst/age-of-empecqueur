3b8d987a91126320a9b60caab3ef772553025322
Glitch (ageof) <none>
2020-12-12 15:08:19 +0000

    👩👧 Checkpoint ./index.html:11367128/666 ./index.js:11367128/5274

--- index.js
+++ index.js
@@ -1,53 +1,110 @@
 // Setup basic express server
 var express = require('express');
 var app = express();
-var server = require('http').createServer(app);
-var io = require('socket.io')(server);
-var port = process.env.PORT || 3000;
-
-server.listen(port, function () {
-  console.log('Server listening at port %d', port);
-});
-
-// Routing
-app.use(express.static('public'));
 
 var players = [];
-var games = [];
+var games = {};
 
 
-io.on('connection', function (socket) {
-  console.log('a user connected');
-  
-  socket.on('create game', function (username) {
-    // creer une partie
-    if(games.length<10){
-      games.push({player1: username})
-      socket.emit('connected', username);
-    }
+
+// Routing
+//app.use(express.static('public'));
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/index.html");
+});
+
+app.get("/createGame", (request, response) => {
+  response.sendFile(__dirname + "/index.html");
+});
+
+//let gameCode = request.data.gameCode
+  var server = require('http').createServer(app);
+  var io = require('socket.io')(server);
+  var port = process.env.PORT || 3000;
+
+  server.listen(port, function () {
+    console.log('Server listening at port %d', port);
   });
   
-  socket.on('add player', function (username) {
-    // we tell the client to execute 'new message'
-    if(players.length<10){
-      players.push(username)
-      socket.broadcast.emit('new connection', players);
-      socket.emit('connected', username);
-    }
-  });
-  
-  socket.on('new message', function (data) {
-    // we tell the client to execute 'new message'
-    socket.broadcast.emit('new message', {
-      username: socket.username,
-      message: data
+  io.on('connection', function (socket) {
+    console.log('a user connected');
+
+    socket.on('create game', function (username) {
+      // creer une partie
+      if(games.length<10){
+        games.push({player1: username})
+        socket.emit('connected', username);
+      }
     });
-  });
-  
-  socket.on('add batiment', function(data) {
-    //io.emit('ping');
-    let details = {nom: "Trinquette", x: 500, y: 100, width: 50, height: 50}
-    io.emit('draw batiment', details);
+
+    socket.on('add player', function (username) {
+      // we tell the client to execute 'new message'
+      if(players.length<10){
+        players.push(username)
+        socket.broadcast.emit('new connection', players);
+        socket.emit('connected', username);
+      }
+    });
+
+    socket.on('new message', function (data) {
+      // we tell the client to execute 'new message'
+      socket.broadcast.emit('new message', {
+        username: socket.username,
+        message: data
+      });
+    });
+
+    socket.on('add batiment', function(data) {
+      //io.emit('ping');
+      let details = {nom: "Trinquette", x: 500, y: 100, width: 50, height: 50}
+      io.emit('draw batiment', details);
+    });
+
   });
 
+app.get("/game", (request, response) => {
+  let gameCode = request.data.gameCode
+  var server = require('http').createServer(app);
+  var io = require('socket.io')(server);
+  var port = process.env.PORT || 3000;
+
+  server.listen(port, function () {
+    console.log('Server listening at port %d', port);
+  });
+  
+  io.on('connection', function (socket) {
+    console.log('a user connected');
+
+    socket.on('create game', function (username) {
+      // creer une partie
+      if(games.length<10){
+        games.push({player1: username})
+        socket.emit('connected', username);
+      }
+    });
+
+    socket.on('add player', function (username) {
+      // we tell the client to execute 'new message'
+      if(players.length<10){
+        players.push(username)
+        socket.broadcast.emit('new connection', players);
+        socket.emit('connected', username);
+      }
+    });
+
+    socket.on('new message', function (data) {
+      // we tell the client to execute 'new message'
+      socket.broadcast.emit('new message', {
+        username: socket.username,
+        message: data
+      });
+    });
+
+    socket.on('add batiment', function(data) {
+      //io.emit('ping');
+      let details = {nom: "Trinquette", x: 500, y: 100, width: 50, height: 50}
+      io.emit('draw batiment', details);
+    });
+
+  });
 });
