bc5c58792ea0b8da2b7c55a072680fdcd5b9e863
Glitch (ageof) <none>
2020-12-24 11:18:20 +0000

    🍄🚉 Checkpoint ./public/script.js:11618920/1238

--- public/script.js
+++ public/script.js
@@ -169,9 +169,14 @@ canvas.addEventListener(
     }
       //
       if(autorisation == false){
-        for (var n = 0.7; n >=0; n=n+0.01) {
-        ctx.fillRect(playerX, playerY, RenduBatiments[batSelect].width, RenduBatiments[batSelect].height);
-        ctx.fillStyle = "rgba(255,0,0,)";
+        var i=0;
+        for (var n = 0.7; n >= 0 ; n=n-0.01){
+         setTimeout(function () {
+            i++;
+            ctx.fillRect(playerX, playerY, RenduBatiments[batSelect].width, RenduBatiments[batSelect].height);
+            ctx.fillStyle = "rgba(255,0,0,"+n.toString();+")";
+         }, 2000 * i);
+        
         }
       }
       if(autorisation == true){
