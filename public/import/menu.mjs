95433427c71aaad3d611336e2b869ccae47ab73b
Glitch (ageof) <none>
2021-01-09 14:47:15 +0000

    🚞✂️ Checkpoint ./public/style.css:11618920/26 ./public/import/menu.mjs:11618920/535

--- public/import/menu.mjs
+++ public/import/menu.mjs
@@ -8,21 +8,15 @@ export function displayMenuBatiments(id, batClick){
         //menu_bat.innerHTML = "le bat sur lequel tu a cliqué est " + batClick;
         map[id].forEach(batiment => {
           if (batiment.nom == batClick) {
-            document.getElementById("menu_bat").style.backgroundImage =
-              "url(" + batiment.image + ")" + " right top height="+batiment.height+"px"+" width="+batiment.height+"px";
             document.getElementById("form_bat").style.opacity = 0.8;
-            document.getElementById("nom_bat").innerHTML = batiment.nom;
-            document.getElementById("lvl_bat").innerHTML =
-              batiment.nom + " de niveau " + batiment.lvl;
+            document.getElementById("nom_bat").innerHTML = batiment.nom+" lvl "+batiment.lvl;
             document.getElementById("lvlUpPrice_bat").innerHTML =
-              "Il vous faudra pièces d'or " +
+              "Coût d'amélioration = " +
               batiment.lvlUpPrice +
-              " pour ameliorer votre " +
-              batiment.nom;
+              " pièces d'or"
             document.getElementById("hp_bat").value = batiment.hp;
             document.getElementById("hp_bat").max = batiment.hpMax;
             document.getElementById("button1").innerHTML = "Ajout de soldat";
-
           }
         });
 }
