a379305ae9d6c41fedc35cc079854869cc2e57c0
Glitch (ageof) <none>
2021-01-10 08:40:19 +0000

    🚴🏂 Checkpoint ./public/script.mjs:11618920/283 ./public/style.css:11618920/50

--- public/script.mjs
+++ public/script.mjs
@@ -103,7 +103,19 @@ bouton1Menu.addEventListener("click", event => {
 //-----------------------------------------------------KEYBOARD----------------------------------------------------------//
 
 document.addEventListener("keydown", keyDownHandler, false);
-function keyDownHandler(e) {}
+function keyDownHandler(e) {
+var keynum;
+if(window.event) // IE
+  {
+  keynum = e.keyCode;
+  }
+else if(e.which) // Netscape/Firefox/Opera
+  {
+  keynum = e.which;
+  }
+//alert(keynum);
+	if (keynum == 27) batSelect = null;
+}
 
 //-------------------------------------------------------MOUSE-----------------------------------------------------------//
 document.addEventListener("mousemove", mouseMoveHandler);
