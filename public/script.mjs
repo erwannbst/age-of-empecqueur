b5f674c08c41ceed218bd7800c3840c51f6866b7
Glitch (ageof) <none>
2021-01-11 15:45:18 +0000

    💂🐥 Checkpoint ./public/script.mjs:11618920/74

--- public/script.mjs
+++ public/script.mjs
@@ -122,8 +122,8 @@ else if(e.which) // Netscape/Firefox/Opera
 //-------------------------------------------------------MOUSE-----------------------------------------------------------//
 document.addEventListener("mousemove", mouseMoveHandler);
 function mouseMoveHandler(e) {
-  playerX = (pageWidth-1900)/2  + (1900*e.pageX)/pageWidth;
-  playerY = (pageHeight-900)/2  + (900*e.pageY)/pageHeight;
+  playerX = (pageWidth-1900)/2  + (1900/pageWidth)*e.pageX;
+  playerY = (pageHeight-900)/2  + (900/pageHeight)*e.pageY;
   document.getElementById("output").innerHTML =
     "Mouse:  <br />" + " x: " + playerX + ", y: " + playerY + "<br />";
 }
