17332a175102b6eb12bd37efa293f9ecf053c9f6
Glitch (ageof) <none>
2021-01-28 16:54:18 +0000

    💴🏙 Checkpoint ./gameValues.js:11367128/11

--- gameValues.js
+++ gameValues.js
@@ -1,6 +1,6 @@
 export const INITIAL_GOLD_AMOUNT = 200;
 export const INTERVAL_GOLD_INCREMENT = 500;
-export const INTERVAL_SEND_MAP = 100;
+export const INTERVAL_SEND_MAP = 50;
 
 export const SOLDIER_VALUES = {
   damage: 1, ms: 4, as: 2, range: 20, hp: 40, cost: 20, createDelay: 1000
